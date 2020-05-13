04c427aceab10bb10fa8dde10fbb17015f5a7e0f
Jason Schwebke <jmschwebke@icloud.com>
2020-05-12 23:47:26 -0400

    non-unique item id update

--- test-cms/test-web/src/main/java/com/jmsweb/auction/entity/Item.java
+++ test-cms/test-web/src/main/java/com/jmsweb/auction/entity/Item.java
@@ -1,12 +1,16 @@
 package com.jmsweb.auction.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Item {
 
     @Id
+    @GeneratedValue
+    private Long id;
+
     private String itemId;
 
     private String description;
@@ -16,6 +20,14 @@ public class Item {
     public Item(String itemId, String description) {
         this.itemId = itemId;
         this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getItemId() {
